57b478751aa31e5c4cc67681c2cb087cd685fa44
Marchello92 <mark.davidovich@gmail.com>
2024-04-01 19:10:03 +0300

    Attempt #3 at fixing DB

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://mark:mark123@cluster0.cm2yvoi.mongodb.net/?retryWrites=true&w=majority&appName=cluster0',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://mark:<password>@cluster0.cm2yvoi.mongodb.net/',
   dbName: process.env.DB_NAME || 'toyDB'
 }
