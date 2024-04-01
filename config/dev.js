54ffee6dbf5cdb067b3537d843e7fd1057f417b7
Marchello92 <mark.davidovich@gmail.com>
2024-04-01 17:33:37 +0300

    Fix bug related to changing databases

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
   dbURL: 'mongodb+srv://mark:mark123@cluster0.cm2yvoi.mongodb.net/?retryWrites=true&w=majority&appName=cluster0',
   // dbURL: 'mongodb://127.0.0.1:27017',
-  dbName: 'toyTest_db',
+  dbName: 'toyDB',
 }
